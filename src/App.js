cd896e575ba504b7bd1cd6bd136649f1dbc7a2fb
insu1170 <insu1170@naver.com>
2023-12-05 21:55:16 +0900

    files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import MainImg from "./component/MainImg";
 import "./App.css";
-
-
+import { Route, Routes } from "react-router-dom";
+import MainBoard from "./component/MainBoard";
 function App() {
   return (
     <div>
-      <MainImg></MainImg>
+      <Routes>
+        <Route path="/" element={<MainImg/>} />
+        <Route path="/Board" element={<MainBoard />} />
+      </Routes>
     </div>
   );
 }
--- src/component/NoteBoard.jsx
+++ src/component/NoteBoard.jsx
@@ -1,10 +1,10 @@
-
+import { Link } from "react-router-dom";
 function NoteBoard(){
 
 
   return(
     <div className="notice">
-      이 곳은 게시판이 들어갈 자리 입니다
+      이 곳은 게시판이 들어갈 자리 입니다       <Link to="Board">+더보기</Link>
     </div>
   )
 
