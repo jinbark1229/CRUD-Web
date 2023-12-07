4ebb3b9168b3f439187bd2b26035f6d11187f4af
jinbark1229 <jinbark1229@naver.com>
2023-12-07 21:20:52 +0900

    파일 추가 입니다.

---
+++ src/page/SLPS.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SLPS(){
+  return(
+    <div>
+      "ALPS는 ‘Advanced Liquid Processing System’의 약자로, 원전 오염수에 있는 방사성 물질을 제거 및 정화하는 다핵종제거설비다. 후쿠시마 원전에서 가동되고 있으며, 원전 오염수 처리 과정 내 일종의 정수기
+      역할을 수행한다. ALPS는 후쿠시마 원전 오염수를 정화하기 위해 개발됐다."
+    </div>
+  );
+}
+
+export default SLPS;
